fe08e7a03887519acaae717a2556e0e1ed4530e9
Anna Henningsen <anna.henningsen@mongodb.com>
2020-10-26 15:04:01 +0100

    Add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+export declare function getDataLake(buildInfo: any): {
+  isDataLake: boolean;
+  dlVersion: string;
+};
+
+export declare function isEnterprise(buildInfo: any): boolean;
+export declare function isAtlas(buildInfo: any): boolean;
+export declare function isLocalhost(buildInfo: any): boolean;
+
+export declare function getGenuineMongoDB(buildInfo: any, cmdLineOpts: any): {
+  isGenuine: boolean;
+  serverName: string;
+};
+
+export declare function getBuildEnv(buildInfo: any): {
+  serverOs: string;
+  serverArch: string;
+};
